b3cff3b0874c97691a2b13b643d9d2abff03b78d
kurjakrichard <kurjak.richard@gmail.com>
2021-12-08 18:55:19 +0100

    first commit

---
+++ lib/first_application.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('Hello Ricsi!');
+  print('Második sor');
+  var alap = myApp();
+  alap.repeat();
+}
+
+class myApp {
+  void repeat() {
+    print('Hello Ricsi!');
+    print('Második sori');
+  }
+}
