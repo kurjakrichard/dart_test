711f6f269d61dfa1c8b94a27f9c0f36b7863d057
Kurják Richárd <kurjak.richard@gmail.com>
2021-12-09 21:08:20 +0100

    Daily change

--- bin/first_application.dart
+++ bin/first_application.dart
@@ -1,22 +1,13 @@
+import 'gyujtemeny_test.dart';
 import 'nullable_test.dart';
 
 void main() {
-  //Ez a rész a nullable változók tesztelésére való
-  var alap = nullableTest();
-  alap.repeat();
-  print('hello');
+  /**Ez a rész a nullable változók tesztelésére való
+  //var alap = nullableTest();
+  //alap.repeat();
+  //print('hello');**/
 
   //Ez a rész a gyűjtemények tesztelésére való
   var lista = gyujtemenyTest();
   lista.repeat();
-}
-
-class gyujtemenyTest {
-  var hello = ['szia', 'szevasz'];
-  var hel = print('helloka');
-
-  void repeat() {
-    print(hello.runtimeType);
-    print(hello);
-  }
 }
---
+++ bin/gyujtemeny_test.dart
@@ -0,0 +1,34 @@
+class gyujtemenyTest {
+  var newList = List.unmodifiable(['szia', 'szevasz']);
+  var hel = ['szia', 'szevasz'];
+  var newMap = {1: 'hello', 2: 'világ'};
+  var newSet = {1, 2, 3};
+  var countries = ['Magyarország', 'Anglia'];
+
+  // Feltételes elem hozzáadás
+  var conditionList = [
+    if (true) {'siker'} else if (false) {'kudarc'}
+  ];
+
+  // Ciklikus elem hozzáadás csak for ciklus while nem nincs break, continue
+  //var hi = [for (var elem in hel()) 'Köszönés: $his'];
+  /** var countryList = [
+    'Országok listája:',
+    for (var country in countries()) 'Ország: $country',
+  ];**/
+  //var hel = print('helloka');
+
+  //
+
+  void repeat() {
+    /** var countryList = [
+      'Országok listája:',
+      for (var country in countries()) 'Ország: $country',
+    ];**/
+    print(newList.runtimeType);
+    //print(hello);
+    hel.add('hi'); //Ez nem működik unmodifiable listán.
+    print(hel);
+    print(conditionList[0].toList());
+  }
+}
