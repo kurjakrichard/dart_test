cd2d2712a3d686f4c47dd957daf3808427487b45
Kurják Richárd <kurjak.richard@gmail.com>
2021-12-09 09:56:24 +0100

    Daily update

--- bin/first_application.dart
+++ bin/first_application.dart
@@ -1,13 +1,22 @@
+import 'nullable_test.dart';
+
 void main() {
-  print('Hello Ricsi!');
-  print('Második sor');
-  var alap = myApp();
+  //Ez a rész a nullable változók tesztelésére való
+  var alap = nullableTest();
   alap.repeat();
+  print('hello');
+
+  //Ez a rész a gyűjtemények tesztelésére való
+  var lista = gyujtemenyTest();
+  lista.repeat();
 }
 
-class myApp {
+class gyujtemenyTest {
+  var hello = ['szia', 'szevasz'];
+  var hel = print('helloka');
+
   void repeat() {
-    print('Hello Ricsi!');
-    print('Második sori');
+    print(hello.runtimeType);
+    print(hello);
   }
 }
